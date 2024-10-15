f643eee105a6c6225faf72f038ae22121341a762
Christopher Goulart <cgoulart35@gmail.com>
2024-10-15 02:58:12 -0400

    Always try to publish (as HTTP handles bad requests)

--- src/NotifyRun.cpp
+++ src/NotifyRun.cpp
@@ -4,19 +4,17 @@ NotifyRun::NotifyRun(LogUtility& logUtility, const String& channelId)
     : logUtility(logUtility), channelId(channelId) {}
 
 int NotifyRun::publish(const String& message) {
-    if (WiFi.status() == WL_CONNECTED) {
-        HTTPClient http;
-        String url = "https://notify.run/" + channelId;
-        http.begin(url);
-        logUtility.loglnInfo("Publishing message to channel " + channelId + ": " + message);
+    HTTPClient http;
+    String url = "https://notify.run/" + channelId;
+    http.begin(url);
+    logUtility.loglnInfo("Publishing message to channel " + channelId + ": " + message);
 
-        int httpResponseCode = http.POST(message);
-        if (httpResponseCode == 200 || httpResponseCode == 201) {
-            logUtility.loglnInfo("Published successfully: " + String(httpResponseCode) + " " + http.getString());
-        } else {
-            logUtility.loglnInfo("Failed to publish: " + String(httpResponseCode) + " " + http.getString());
-        }
-        http.end();
-        return httpResponseCode;
+    int httpResponseCode = http.POST(message);
+    if (httpResponseCode == 200 || httpResponseCode == 201) {
+        logUtility.loglnInfo("Published successfully: " + String(httpResponseCode) + " " + http.getString());
+    } else {
+        logUtility.loglnInfo("Failed to publish: " + String(httpResponseCode) + " " + http.getString());
     }
+    http.end();
+    return httpResponseCode;
 }
